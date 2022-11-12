77fe9c78b1779e9db5b87f05e75b2734446a0bc2
Sasha Samoilov <samsiki50@gmail.com>
2022-11-13 01:36:40 +0300

    new lab

---
+++ Labs/Lab08/Demo/DemoArray.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class DemoArray
+    {
+        int[] MyArray;//закрытый массив
+        string name; //закрытое поле
+        public DemoArray(int size, int x, string name)//конструктор
+        {
+            MyArray = new int[size];
+            this.name = name;
+            for (int i = 0; i < size; ++i) MyArray[i] = x;
+        }
+        public int[] myArray
+        {
+            get
+            {
+                return MyArray;
+            }
+            set
+            {
+                MyArray = value;
+            }
+        }
+        public void Print()//метод
+        {
+            Console.Write(name + ":");
+            foreach (int a in MyArray) Console.Write(a + " ");
+            Console.WriteLine();
+        }
+        public int LengthN //свойство
+        {
+            get { return MyArray.Length; }
+        }
+        public static DemoArray operator +(DemoArray a)
+        {
+            for(int i = 0;i < a.myArray.Length;i++)
+            {
+                a.myArray[i] = Math.Abs(a.myArray[i]); 
+            }
+            return a;
+        }
+        public static DemoArray operator -(DemoArray a)
+        {
+            for (int i = 0; i < a.myArray.Length; i++)
+            {
+                a.myArray[i] = a.myArray[i] - 1;
+            }
+            return a;
+        }
+        public static DemoArray operator -(DemoArray a,int b)
+        {
+            for (int i = 0; i < a.myArray.Length; i++)
+            {
+                a.myArray[i] = a.MyArray[i] - b;
+            }
+            return a;
+        }
+        public static bool operator &(DemoArray a,DemoArray b)
+        {
+            for (int i = 0; i < a.myArray.Length; i++)
+            {
+                if (a.myArray[i] != b.myArray[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        ~DemoArray()//деструктор
+        {
+            Console.WriteLine(" сработал деструктор для объекта " +this.name);
+        }
+
+    }
+}
---
+++ Labs/Lab08/Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoArray a = new DemoArray(5, -10, "десять");
+            a.Print();
+            Console.WriteLine("все элементы массива преобразуются в положительные");
+            a=+a;
+            a.Print();
+            Console.WriteLine("все элементы массива уменьшаются на единицу");
+            a = -a;
+            a.Print();
+            Console.WriteLine("из всех элементов массива вычитается заданное число");
+            a = a - 1;
+            a.Print();
+            DemoArray b = new DemoArray(5, 8, "пять");
+            b.Print();
+            if (a & b)
+            {
+                Console.WriteLine("схожи");
+            }
+            else
+            {
+                Console.WriteLine("разные");
+            }
+            Console.ReadKey();
+        }
+    }
+}
