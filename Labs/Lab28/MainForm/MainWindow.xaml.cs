4dad31de34eaa35ad63de29f5806676ebb1db0f3
Sasha <samsiki50@gmail.com>
2023-05-01 18:34:52 +0300

    New Lab

---
+++ Labs/Lab28/MainForm/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainForm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            TaskOne one = new TaskOne();
+            one.Show();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            TaskTwo two = new TaskTwo();
+            two.Show();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            TaskThree three = new TaskThree();
+            three.Show();
+        }
+    }
+}
---
+++ Labs/Lab28/MainForm/TaskThree.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainForm
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskThree.xaml
+    /// </summary>
+    public partial class TaskThree : Window
+    {
+        public TaskThree()
+        {
+            InitializeComponent();
+            var fromPoint = new Point(0.3, 0.3);
+            var toPoint = new Point(0.7, 0.7);
+            var animation = new PointAnimation(fromPoint, toPoint, new Duration(TimeSpan.FromSeconds(2)))
+            {
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+            light.BeginAnimation(RadialGradientBrush.GradientOriginProperty, animation);
+
+        }
+    }
+}
